49c3bee889e1e75a6b35e0de7f8004a9e8bf628c
butlerpaul1 <c14521457@mydit.ie>
2018-03-29 22:37:23 +0100

    Added mLab requests, to allow user to add fav and completed walks

---
+++ app/src/main/java/com/example/paul/myapplication/api/model/User.java
@@ -0,0 +1,37 @@
+package com.example.paul.myapplication.api.model;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by butle on 3/29/2018.
+ */
+
+public class User {
+
+
+    @SerializedName("email")
+    private String email;
+    @SerializedName("completedWalks")
+    private String[] completedWalks;
+    @SerializedName("favWalks")
+    private String[] favWalks;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String[] getCompletedWalks() {
+        return completedWalks;
+    }
+
+    public String[] getFavWalks() {
+        return favWalks;
+    }
+
+    public User(String email, String[] completedWalks, String[] favWalks) {
+        this.email = email;
+        this.completedWalks = completedWalks;
+        this.favWalks = favWalks;
+    }
+
+}
